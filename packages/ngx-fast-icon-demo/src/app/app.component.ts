7736909e5380f64ed0d9d33ffc4c172e0d3096d1
Chris <chris@deep-blue.io>
2023-03-17 15:11:16 +0100

    docs: make mobile responsive navbar

--- packages/ngx-fast-icon-demo/src/app/app.component.ts
+++ packages/ngx-fast-icon-demo/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 
 import { filter, map, startWith } from 'rxjs';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'ngx-fast-icon-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   links = [
     'description',
     'fast-svg',
@@ -19,13 +20,28 @@ export class AppComponent {
     'ant',
   ];
   readonly queryParams = this.activatedRoute.queryParams;
-
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
   navClass$ = this.router.events.pipe(
     filter((e) => e instanceof NavigationEnd),
     startWith({ urlAfterRedirects: this.router.url }),
     map((e) => this.getClassFromUrl((e as NavigationEnd).urlAfterRedirects))
   );
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   getClassFromUrl(url: string): string {
     if (url.includes('angular')) {
