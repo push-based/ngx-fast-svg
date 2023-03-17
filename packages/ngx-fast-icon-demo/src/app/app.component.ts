1671e14135a758fadef8b3183f72e928b728befb
Chris <chris@deep-blue.io>
2023-03-17 18:40:57 +0100

    Revert "docs(refactor): convert addListener to addEventListener"
    This reverts commit 3deb0f4dcee9f010dc3d2f0ee28919c6f9d1a32f.

--- packages/ngx-fast-icon-demo/src/app/app.component.ts
+++ packages/ngx-fast-icon-demo/src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnDestroy {
   ];
   readonly queryParams = this.activatedRoute.queryParams;
   mobileQuery: MediaQueryList;
-  private readonly _mobileQueryListener: () => void;
+  private _mobileQueryListener: () => void;
   navClass$ = this.router.events.pipe(
     filter((e) => e instanceof NavigationEnd),
     startWith({ urlAfterRedirects: this.router.url }),
@@ -36,11 +36,11 @@ export class AppComponent implements OnDestroy {
   ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addEventListener("change",this._mobileQueryListener);
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
   ngOnDestroy(): void {
-    this.mobileQuery.removeEventListener("change", this._mobileQueryListener);
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
   getClassFromUrl(url: string): string {
