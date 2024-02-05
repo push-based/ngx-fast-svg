544b273799ddab01b51b4f4fcb6a06e674711517
ChristopherPHolder <toholderandrews@gmail.com>
2024-02-05 10:20:05 +0100

    fix: fix code

--- packages/ngx-fast-icon-demo/src/app/app.component.ts
+++ packages/ngx-fast-icon-demo/src/app/app.component.ts
@@ -1,40 +1,39 @@
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 
-import { filter, map, startWith } from 'rxjs';
+import { filter, map } from 'rxjs';
 import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'ngx-fast-icon-root',
   template: `
-    <div class="root" [ngClass]="(navClass$ | async) || ''">
-      <mat-toolbar class="header-toolbar sanity-check" [class.header-is-mobile]="mobileQuery.matches">
+    <div class="root" [class]="rootClass$ | async">
+      <mat-toolbar class="header-toolbar sanity-check" [class.header-is-mobile]="isMobile">
         <a (click)="snav.toggle()">
           <fast-svg id="nav-icon" height="48" width="48" name="menu" />
         </a>
         <h1 class="title">ngx-fast-svg</h1>
       </mat-toolbar>
       <div class="row">
-        <mat-sidenav-container [style.marginTop.px]="mobileQuery.matches ? 56 : 0">
-          <mat-sidenav #snav mode="side" [opened]="!mobileQuery.matches" [fixedInViewport]="mobileQuery.matches" fixedTopGap="56">
+        <mat-sidenav-container [style.marginTop.px]="isMobile ? 56 : 0">
+          <mat-sidenav #snav mode="side" [opened]="!isMobile" [fixedInViewport]="isMobile" fixedTopGap="56">
             <div class="sidebar">
               <div class="header">
-                <a
-                  class="link"
-                  *ngFor="let link of links"
-                  [routerLinkActive]="'active'"
-                  [ngClass]="link"
-                  [routerLink]="link"
-                  [queryParams]="queryParams | async"
-                >
-                  {{ link }}
-                </a>
+                @for (link of links; track $index) {
+                  <a
+                    class="link"
+                    [routerLinkActive]="'active'"
+                    [ngClass]="link"
+                    [routerLink]="link"
+                    [queryParams]="queryParams$ | async"
+                  >{{ link }}</a>
+                }
               </div>
             </div>
           </mat-sidenav>
           <mat-sidenav-content>
             <div class="content">
-              <router-outlet></router-outlet>
+              <router-outlet />
             </div>
           </mat-sidenav-content>
         </mat-sidenav-container>
@@ -43,7 +42,17 @@ import {MediaMatcher} from '@angular/cdk/layout';
   `,
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+
+  readonly queryParams$ = this.route.queryParams;
+  readonly isMobile = inject(MediaMatcher).matchMedia('(max-width: 600px)').matches;
+  readonly rootClass$ = this.router.events.pipe(
+    filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+    map((e) => e.urlAfterRedirects.split('?')[0].replace('/', ''))
+  )
+
   readonly links = [
     'description',
     'fast-svg',
@@ -53,58 +62,4 @@ export class AppComponent implements OnDestroy {
     'angular',
     'ant',
   ];
-  readonly queryParams = this.activatedRoute.queryParams;
-  mobileQuery: MediaQueryList;
-  private readonly _mobileQueryListener: () => void;
-  navClass$ = this.router.events.pipe(
-    filter((e) => e instanceof NavigationEnd),
-    startWith({ urlAfterRedirects: this.router.url }),
-    map((e) => this.getClassFromUrl((e as NavigationEnd).urlAfterRedirects))
-  );
-
-  constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    changeDetectorRef: ChangeDetectorRef,
-    media: MediaMatcher,
-  ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
-
-  getClassFromUrl(url: string): string {
-    if (url.includes('angular')) {
-      return 'angular';
-    }
-
-    if (url.includes('material')) {
-      return 'material';
-    }
-
-    if (url.includes('ant')) {
-      return 'ant';
-    }
-    if (url.includes('fast-svg')) {
-      return 'fast-svg';
-    }
-
-    if (url.includes('font-awesome')) {
-      return 'font-awesome';
-    }
-
-    if (url.includes('ionic')) {
-      return 'ionic';
-    }
-
-    if (url.includes('description')) {
-      return 'description';
-    }
-
-    return '';
-  }
 }
--- packages/ngx-fast-icon-demo/src/app/app.module.ts
+++ packages/ngx-fast-icon-demo/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import {
+  provideRouter,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+  withRouterConfig
+} from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule,  } from '@ionic/angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { FastSvgComponent, provideFastSVG } from '@push-based/ngx-fast-svg';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -12,12 +17,11 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { AngularSvgIconModule } from 'angular-svg-icon';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CommonModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
@@ -27,51 +31,54 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
     HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(),
-    AngularSvgIconModule.forRoot(),
-    RouterModule.forRoot(
-      [
-        {
-          path: '',
-          pathMatch: 'full',
-          redirectTo: 'description'
-        },
-        {
-          path: 'description',
-          loadComponent: () => import('./misc/description.component').then((c) => c.DescriptionComponent)
-        },
-        {
-          path: 'angular',
-          loadComponent: () => import('./comparison/angular.component').then((c) => c.AngularComponent)
-        },
-        {
-          path: 'material',
-          loadComponent: () => import('./comparison/material.component').then((c) => c.MaterialComponent)
-        },
-        {
-          path: 'ant',
-          loadComponent: () => import('./comparison/ant.component').then((c) => c.AntComponent)
-        },
-        {
-          path: 'ionic',
-          loadComponent: () => import('./comparison/ionic.component').then((c) => c.IonicComponent)
-        },
-        {
-          path: 'font-awesome',
-          loadComponent: () => import('./comparison/font-awesome.component').then((c) => c.FontAwesomeComponent)
-        },
-        {
-          path: 'fast-svg',
-          loadComponent: () => import('./comparison/fast-icon.component').then((c) => c.FastIconRouteComponent)
-        }
-      ],
-      { initialNavigation: 'enabledBlocking' }
-    ),
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     FastSvgComponent
   ],
   providers: [
     provideFastSVG({
       url: (name: string) => `assets/svg-icons/${name}.svg`,
     }),
+    provideAngularSvgIcon(),
+    provideRouter(
+      [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'description'
+      },
+      {
+        path: 'description',
+        loadComponent: () => import('./misc/description.component').then((c) => c.DescriptionComponent)
+      },
+      {
+        path: 'angular',
+        loadComponent: () => import('./comparison/angular.component').then((c) => c.AngularComponent)
+      },
+      {
+        path: 'material',
+        loadComponent: () => import('./comparison/material.component').then((c) => c.MaterialComponent)
+      },
+      {
+        path: 'ant',
+        loadComponent: () => import('./comparison/ant.component').then((c) => c.AntComponent)
+      },
+      {
+        path: 'ionic',
+        loadComponent: () => import('./comparison/ionic.component').then((c) => c.IonicComponent)
+      },
+      {
+        path: 'font-awesome',
+        loadComponent: () => import('./comparison/font-awesome.component').then((c) => c.FontAwesomeComponent)
+      },
+      {
+        path: 'fast-svg',
+        loadComponent: () => import('./comparison/fast-icon.component').then((c) => c.FastIconRouteComponent)
+      }
+    ],
+      withRouterConfig({ urlUpdateStrategy: 'eager' })
+    )
   ],
   bootstrap: [AppComponent],
 })
