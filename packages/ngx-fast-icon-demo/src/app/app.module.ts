ee1a13096fc1a156a5e6050815530c976200bae6
ChristopherPHolder <toholderandrews@gmail.com>
2024-02-05 08:23:35 +0100

    fix: add svg and style

--- packages/ngx-fast-icon-demo/src/app/app.module.ts
+++ packages/ngx-fast-icon-demo/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { DescriptionComponent } from './routes/description/description.component
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideFastSVG } from '@push-based/ngx-fast-svg';
+import { FastSvgComponent, provideFastSVG } from '@push-based/ngx-fast-svg';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
@@ -32,66 +32,66 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
         {
           path: '',
           pathMatch: 'full',
-          redirectTo: 'description',
+          redirectTo: 'description'
         },
         {
           path: 'ngx-fast-icon',
           loadChildren: () =>
             import('./misc/ngx-fast-svg/fast-svg-list.module').then(
               (m) => m.FastSvgListModule
-            ),
+            )
         },
         {
           path: 'icon',
           loadChildren: () =>
             import('./misc/angular-svg-icon/angular-svg-icon-list.module').then(
               (m) => m.AngularSvgIconListModule
-            ),
+            )
         },
         {
           path: 'description',
-          component: DescriptionComponent,
+          component: DescriptionComponent
         },
         {
           path: 'angular',
           loadChildren: () =>
             import('./comparison/angular/angular.module').then(
               (m) => m.AngularModule
-            ),
+            )
         },
         {
           path: 'material',
           loadChildren: () =>
             import('./comparison/material/material.module').then(
               (m) => m.MaterialComponentModule
-            ),
+            )
         },
         {
           path: 'ant',
           loadChildren: () =>
-            import('./comparison/ant/ant.module').then((m) => m.AntModule),
+            import('./comparison/ant/ant.module').then((m) => m.AntModule)
         },
         {
           path: 'ionic',
           loadChildren: () =>
             import('./comparison/ionic/ionic.module').then(
               (m) => m.IonicComponentModule
-            ),
+            )
         },
         {
           path: 'font-awesome',
           loadChildren: () =>
             import('./comparison/font-awesome/font-awesome.module').then(
               (m) => m.FontAwesomeRouteModule
-            ),
+            )
         },
         {
           path: 'fast-svg',
           loadChildren: () =>
             import('./comparison/fast-icon/fast-icon.module').then(
               (m) => m.FastIconRouteModule
-            ),
-        },
+            )
+        }
         /* {
         path:'angular-material-icon',
         loadChildren: () => import('./angular-material-icons-list/angular-material-icons-list.module').then(m => m.AngularMaterialIconListModule)
@@ -99,6 +99,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
       ],
       { initialNavigation: 'enabledBlocking' }
     ),
+    FastSvgComponent
   ],
   providers: [
     provideFastSVG({
